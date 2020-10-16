ae5c4d09b5e1d5999c12b527eb49772b7fbd51df
milos381 <milos_popovic@outlook.com>
2020-10-16 19:06:05 -0400

    finish user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,33 @@ class UsersController < ApplicationController
             redirect '/users/signup'
         else
             @user = User.create(username: params[:username], password: params[:password])
-            
+
             session[:user_id] = @user.id
             redirect '/items'
+        end
+    end
+    get '/login' do
+        if !logged_in?
+            erb :'users/login'
+          else
+            redirect to '/items'
+        end
+    end
+    post '/login' do
+        @user = User.find_by(:username => params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect to '/items'
+        else
+            redirect "/users/signup"
+        end
+    end
+    get "/logout" do
+        if logged_in?
+            session.destroy
+            redirect to '/login'
+        else
+            redirect to '/'
         end
     end
 end
