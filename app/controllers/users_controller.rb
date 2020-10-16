b962006c9e9764298f9273f16901e35ffe715395
milos381 <milos_popovic@outlook.com>
2020-10-16 18:40:19 -0400

    finish signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,25 @@
 class UsersController < ApplicationController
-
+    get '/users/:slug' do
+        @user = User.find_by_slug(params[:slug])
+        erb :'users/show'
+      end
+    get "/signup" do
+        
+        if !logged_in?
+            erb :'/users/signup'
+          else
+            redirect to '/items'
+        end
+    end
+    post '/signup' do
+        
+        if params[:email] == "" || params[:username] == "" || params[:password] == ""
+            redirect '/users/signup'
+        else
+            @user = User.create(username: params[:username], password: params[:password])
+            
+            session[:user_id] = @user.id
+            redirect '/items'
+        end
+    end
 end
