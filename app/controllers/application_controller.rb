332762ec63d3b1e2e4dac2fc2ba0d7c60fc370ed
milos381 <milos_popovic@outlook.com>
2020-10-16 18:30:29 -0400

    finish shell

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,23 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "bucket_secret"
+  end
+  
+  get "/" do
+    erb :index
   end
 
-  get "/" do
-    erb :welcome
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
---
+++ db/migrate/002_create_items_table.rb
@@ -0,0 +1,10 @@
+class CreateItemsTable < ActiveRecord::Migration
+    def change
+      create_table :items do |t|
+        t.text :content
+        t.integer :user_id
+  
+        t.timestamps null: false
+      end
+    end
+  end
