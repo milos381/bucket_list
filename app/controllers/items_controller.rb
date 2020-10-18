61e7fd71574f53ffe0e88431bc357deb58aa6b40
milos381 <milos_popovic@outlook.com>
2020-10-18 16:16:16 -0400

    finish

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,6 +40,49 @@ class ItemsController < ApplicationController
           erb :'items/show_item'
         else
           redirect to '/login'
+        end 
+    end
+    get '/items/:id/edit' do
+        if logged_in?
+          @item = Item.find_by_id(params[:id])
+          if @item && @item.user == current_user
+            erb :'items/edit_item'
+          else
+            redirect to '/items'
+          end
+        else
+          redirect to '/login'
         end
+    end
+    patch '/items/:id' do
+        if logged_in?
+          if params[:content] == ""
+            redirect to "/items/#{params[:id]}/edit"
+          else
+            @item = Item.find_by_id(params[:id])
+            if @item && @item.user == current_user
+              if @item.update(content: params[:content])
+                redirect to "/items/#{@item.id}"
+              else
+                redirect to "/items/#{@item.id}/edit"
+              end
+            else
+              redirect to '/items'
+            end
+          end
+        else
+          redirect to '/login'
+        end
+  end
+  delete '/items/:id/delete' do
+    if logged_in?
+      @item = Item.find_by_id(params[:id])
+      if @item && @item.user == current_user
+        @item.delete
       end
-end
+      redirect to '/items'
+      else
+        redirect to '/login'
+      end
+    end
+  end
